17786e9eb3fcfbcf149b4b0deef4472584e36c6e
unknown <m.walter-dev@gmx.de>
2023-02-03 15:00:23 +0100

    merge

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -3,7 +3,19 @@ class MovableObject {
    y = 400;
    img;
 
+   loadImage(path)
+   {
+      this.img = new Image();
+
+
+
    moveRight() {
         console.log('Moving right')
+   }
+
+   moveLeft() {
+        console.log('Moving left')
+   }
+
    }
 }
